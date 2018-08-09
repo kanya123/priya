797844d8379224a149fffc3f9e4da95cfe113302
kanya123 <41769933+kanya123@users.noreply.github.com>
2018-08-09 12:26:35 +0530

    Create 28.java

---
+++ 28.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a[];
+	int n,i;
+	Scanner s=new Scanner(System.in);
+	n=s.nextInt();
+	a=new int[n];
+	for(i=0;i<n;i++)
+	a[i]=s.nextInt();
+	for(i=0;i<n;i++)
+	System.out.println(a[i]+" "+i+" ");
+	
+	}
+}
