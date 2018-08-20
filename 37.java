3a464368c0e64aa320ba4ef6acc52e67fd93901b
kanya123 <41769933+kanya123@users.noreply.github.com>
2018-08-20 13:00:16 +0530

    Create 37.java

---
+++ 37.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+class Ideone
+
+{
+public static void main (String[] args) throws java.lang.Exception
+
+	{
+		
+int a,b,temp;
+		
+
+Scanner sc=new Scanner(System.in);
+		
+a=sc.nextInt();
+		
+b=sc.nextInt();
+		
+temp=a;
+		
+a=b;
+		
+b=temp;
+		
+System.out.println("After swapping\na="+a+"\nb="+b);
+	
+}
+
+}
